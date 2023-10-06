c31716da9c2335a91a2affc9b358bcc00072ff4b
Hautier <thomas.hautier@hotmail.fr>
2023-10-06 10:13:45 +0200

    build failed with this method

--- build-jpeg-recompress-bin.nix
+++ build-jpeg-recompress-bin.nix
@@ -5,26 +5,11 @@ jpeg-recompress-bin = pkgs.runCommand "jpeg-recompress-bin" { } ''
   mkdir -p $out/jpeg-recompress-bin/vendor/linux
   cp ${dataPath}/jpeg-recompress-bin/vendor/linux/jpeg-recompress $out/jpeg-recompress-bin/vendor/linux
 '';
-
-src = ./compress-images-data/jpeg-recompress-bin;
- packageLock = builtins.fromJSON (builtins.readFile (src + "/package-lock.json"));
-
-  # Create an array of all (meaningful) dependencies
-  deps = builtins.attrValues (removeAttrs packageLock.dependencies [ "" ]);
-
-  # Turn each dependency into a fetchurl call
-  tarballs = map (p: pkgs.fetchurl { url = p.resolved; hash = p.integrity; }) deps;
-
-  # Write a file with the list of tarballs
-  tarballsFile = pkgs.writeTextFile {
-    name = "tarballs";
-    text = builtins.concatStringsSep "\n" tarballs;
-  };
 in
 buildNpmPackage rec {
   pname = "jpeg-recompress-bin";
   version = "5.1.2";
-  test = pkgs.copyPathToStore ./compress-images-data;
+  rootPackagesDir = pkgs.copyPathToStore ./packages-to-patch;
 
 
   src = fetchFromGitHub {
@@ -37,19 +22,22 @@ buildNpmPackage rec {
 
 
    prePatch = ''
-   cp ${test}/jpeg-recompress-bin/lib/local-deps.patch local-deps.patch;ls;
+   echo "node -v"
+   cp ${rootPackagesDir}/jpeg-recompress-bin/lib/index.patch index.patch;ls;
    '';
    postPatch = ''
-    cp  ${test}/jpeg-recompress-bin/package-lock.json package-lock.json;
+    echo "LIST OF SCRIPT"
+    npm run
+    cp  ${rootPackagesDir}/jpeg-recompress-bin/package-lock.json package-lock.json;
     ls;
    '';
   npmDepsHash = "sha256-Pp3QgZPGKc0EVzJ3LcWXpQ3h5i9pvtEUOM0QvpHSQOo=";
 
-  patches = ["./local-deps.patch"];
+  patches = ["${rootPackagesDir}/jpeg-recompress-bin/lib/index.patch" "${rootPackagesDir}/jpeg-recompress-bin/package.patch"];
 
   # The prepack script runs the build script, which we'd rather do in the build phase.
   npmPackFlags = [ "--ignore-scripts --loglevel verbose" ];
-  npmBuildFlags = [ "--loglevel verbose" ];
+
   makeCacheWritable = true;
 
   NODE_OPTIONS = "--openssl-legacy-provider";
